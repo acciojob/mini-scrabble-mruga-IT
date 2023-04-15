edb9ce494194e5378567970aa52ea60f6fc3c7f8
mruga-IT <124659354+mruga-IT@users.noreply.github.com>
2023-04-15 20:48:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+// JavaScript code
+function countLetters() {
+  const word = document.getElementById("evaluatedText").value;
+  const letterCount = word.length;
+  document.getElementById("letterCount").textContent = letterCount;
+}
